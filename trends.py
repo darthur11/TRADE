cfd84b0b3e86e1fd1013efe5dc9c0497bf9cae1a
darthur11 <darthur11@users.noreply.github.com>
2019-08-12 22:01:24 +0600

    First commit

---
+++ trends.py
@@ -0,0 +1,352 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 12 13:26:49 2019
+
+@author: Artur.Dossatayev
+"""
+
+def supres(low, high, n=28, min_touches=2, stat_likeness_percent=1.5, bounce_percent=5):
+    """Support and Resistance Testing
+    Identifies support and resistance levels of provided price action data.
+    Args:
+        n(int): Number of frames to evaluate
+        low(pandas.Series): A pandas Series of lows from price action data.
+        high(pandas.Series): A pandas Series of highs from price action data.
+        min_touches(int): Minimum # of touches for established S&R.
+        stat_likeness_percent(int/float): Acceptable margin of error for level.
+        bounce_percent(int/float): Percent of price action for established bounce.
+    
+    ** Note **
+        If you want to calculate support and resistance without regard for
+        candle shadows, pass close values for both low and high.
+    Returns:
+        sup(float): Established level of support or None (if no level)
+        res(float): Established level of resistance or None (if no level)
+    """
+    import pandas as pd
+    import numpy as np
+
+    # Collapse into dataframe
+    df = pd.concat([high, low], keys = ['high', 'low'], axis=1)
+    df['sup'] = pd.Series(np.zeros(len(low)))
+    df['res'] = pd.Series(np.zeros(len(low)))
+    df['sup_break'] = pd.Series(np.zeros(len(low)))
+    df['sup_break'] = 0
+    df['res_break'] = pd.Series(np.zeros(len(high)))
+    df['res_break'] = 0
+    
+    for x in range((n-1)+n, len(df)):
+        # Split into defined timeframes for analysis
+        tempdf = df[x-n:x+1]
+        
+        # Setting default values for support and resistance to None
+        sup = None
+        res = None
+        
+        # Identifying local high and local low
+        maxima = tempdf.high.max()
+        minima = tempdf.low.min()
+        
+        # Calculating distance between max and min (total price movement)
+        move_range = maxima - minima
+        
+        # Calculating bounce distance and allowable margin of error for likeness
+        move_allowance = move_range * (stat_likeness_percent / 100)
+        bounce_distance = move_range * (bounce_percent / 100)
+        
+        # Test resistance by iterating through data to check for touches delimited by bounces
+        touchdown = 0
+        awaiting_bounce = False
+        for y in range(0, len(tempdf)):
+            if abs(maxima - tempdf.high.iloc[y]) < move_allowance and not awaiting_bounce:
+                touchdown = touchdown + 1
+                awaiting_bounce = True
+            elif abs(maxima - tempdf.high.iloc[y]) > bounce_distance:
+                awaiting_bounce = False
+        if touchdown >= min_touches:
+            res = maxima
+        # Test support by iterating through data to check for touches delimited by bounces
+        touchdown = 0
+        awaiting_bounce = False
+        for y in range(0, len(tempdf)):
+            if abs(tempdf.low.iloc[y] - minima) < move_allowance and not awaiting_bounce:
+                touchdown = touchdown + 1
+                awaiting_bounce = True
+            elif abs(tempdf.low.iloc[y] - minima) > bounce_distance:
+                awaiting_bounce = False
+        if touchdown >= min_touches:
+            sup = minima
+        if sup:
+            df['sup'].iloc[x] = sup
+        if res:
+            df['res'].iloc[x] = res
+    res_break_indices = list(df[(np.isnan(df['res']) & ~np.isnan(df.shift(1)['res'])) & (df['high'] > df.shift(1)['res'])].index)
+    for index in res_break_indices:
+        df['res_break'].at[index] = 1
+    sup_break_indices = list(df[(np.isnan(df['sup']) & ~np.isnan(df.shift(1)['sup'])) & (df['low'] < df.shift(1)['sup'])].index)
+    for index in sup_break_indices:
+        df['sup_break'].at[index] = 1
+    ret_df = pd.concat([df['sup'], df['res'], df['sup_break'], df['res_break']], keys = ['sup', 'res', 'sup_break', 'res_break'], axis=1)
+    return ret_df
+
+
+
+def gentrends(x, window=1/3.0, charts=True):
+    """
+    Returns a Pandas dataframe with support and resistance lines.
+    :param x: One-dimensional data set
+    :param window: How long the trendlines should be. If window < 1, then it
+                   will be taken as a percentage of the size of the data
+    :param charts: Boolean value saying whether to print chart to screen
+    """
+
+    import numpy as np
+    import pandas as pd
+
+    x = np.array(x)
+
+    if window < 1:
+        window = int(window * len(x))
+
+    max1 = np.where(x == max(x))[0][0]  # find the index of the abs max
+    min1 = np.where(x == min(x))[0][0]  # find the index of the abs min
+
+    # First the max
+    if max1 + window > len(x):
+        max2 = max(x[0:(max1 - window)])
+    else:
+        max2 = max(x[(max1 + window):])
+
+    # Now the min
+    if min1 - window < 0:
+        min2 = min(x[(min1 + window):])
+    else:
+        min2 = min(x[0:(min1 - window)])
+
+    # Now find the indices of the secondary extrema
+    max2 = np.where(x == max2)[0][0]  # find the index of the 2nd max
+    min2 = np.where(x == min2)[0][0]  # find the index of the 2nd min
+
+    # Create & extend the lines
+    maxslope = (x[max1] - x[max2]) / (max1 - max2)  # slope between max points
+    minslope = (x[min1] - x[min2]) / (min1 - min2)  # slope between min points
+    a_max = x[max1] - (maxslope * max1)  # y-intercept for max trendline
+    a_min = x[min1] - (minslope * min1)  # y-intercept for min trendline
+    b_max = x[max1] + (maxslope * (len(x) - max1))  # extend to last data pt
+    b_min = x[min1] + (minslope * (len(x) - min1))  # extend to last data point
+    maxline = np.linspace(a_max, b_max, len(x))  # Y values between max's
+    minline = np.linspace(a_min, b_min, len(x))  # Y values between min's
+
+    # OUTPUT
+    trends = np.transpose(np.array((x, maxline, minline)))
+    trends = pd.DataFrame(trends, index=np.arange(0, len(x)),
+                          columns=['Data', 'Max Line', 'Min Line'])
+
+    if charts is True:
+        from matplotlib.pyplot import plot, grid, show
+        plot(trends)
+        grid()
+        show()
+
+    return trends, maxslope, minslope
+
+def segtrends(x, segments=2, charts=True):
+    """
+    Turn minitrends to iterative process more easily adaptable to
+    implementation in simple trading systems; allows backtesting functionality.
+    :param x: One-dimensional data set
+    :param window: How long the trendlines should be. If window < 1, then it
+                   will be taken as a percentage of the size of the data
+    :param charts: Boolean value saying whether to print chart to screen
+    """
+
+    import numpy as np
+    y = np.array(x)
+    # Implement trendlines
+    segments = int(segments)
+    maxima = np.ones(segments)
+    minima = np.ones(segments)
+    segsize = int(len(y)/segments)
+    for i in range(1, segments+1):
+        ind2 = i*segsize
+        ind1 = ind2 - segsize
+        maxima[i-1] = max(y[ind1:ind2])
+        minima[i-1] = min(y[ind1:ind2])
+
+    # Find the indexes of these maxima in the data
+    x_maxima = np.ones(segments)
+    x_minima = np.ones(segments)
+    for i in range(0, segments):
+        x_maxima[i] = np.where(y == maxima[i])[0][0]
+        x_minima[i] = np.where(y == minima[i])[0][0]
+
+    if charts:        
+        import matplotlib.pyplot as plt
+        plt.figure()
+        plt.plot(y, linewidth=1)
+        plt.grid(True)
+
+    for i in range(0, segments-1):
+        maxslope = (maxima[i+1] - maxima[i]) / (x_maxima[i+1] - x_maxima[i])
+        a_max = maxima[i] - (maxslope * x_maxima[i])
+        b_max = maxima[i] + (maxslope * (len(y) - x_maxima[i]))
+        maxline = np.linspace(a_max, b_max, len(y))
+
+        minslope = (minima[i+1] - minima[i]) / (x_minima[i+1] - x_minima[i])
+        a_min = minima[i] - (minslope * x_minima[i])
+        b_min = minima[i] + (minslope * (len(y) - x_minima[i]))
+        minline = np.linspace(a_min, b_min, len(y))
+
+        if charts:
+            plt.plot(maxline, 'g', linewidth=0.35)
+            plt.plot(minline, 'r', linewidth=.35)
+
+
+    if charts:
+        #plt.show()
+        plt.ylim(-2, 300)
+        plt.savefig('C:/Python/diver/chart.png', dpi = 600)
+        plt.close()
+
+
+    # OUTPUT
+    return x_maxima, maxima, x_minima, minima
+
+segtrends(data['Close'], segments = 12, charts = True)
+
+
+
+def minitrends(x, window=20, charts=True):
+    """
+    Turn minitrends to iterative process more easily adaptable to
+    implementation in simple trading systems; allows backtesting functionality.
+    :param x: One-dimensional data set
+    :param window: How long the trendlines should be. If window < 1, then it
+                   will be taken as a percentage of the size of the data
+    :param charts: Boolean value saying whether to print chart to screen
+    """
+
+    import numpy as np
+
+    y = np.array(x)
+    if window < 1:  # if window is given as fraction of data length
+        window = float(window)
+        window = int(window * len(y))
+    x = np.arange(0, len(y))
+    dy = y[window:] - y[:-window]
+    crit = dy[:-1] * dy[1:] < 0
+
+    # Find whether max's or min's
+    maxi = (y[x[crit]] - y[x[crit] + window] > 0) & \
+           (y[x[crit]] - y[x[crit] - window] > 0) * 1
+    mini = (y[x[crit]] - y[x[crit] + window] < 0) & \
+           (y[x[crit]] - y[x[crit] - window] < 0) * 1
+    maxi = maxi.astype(float)
+    mini = mini.astype(float)
+    maxi[maxi == 0] = np.nan
+    mini[mini == 0] = np.nan
+    xmax = x[crit] * maxi
+    xmax = xmax[~np.isnan(xmax)]
+    xmax = xmax.astype(int)
+    xmin = x[crit] * mini
+    xmin = xmin[~np.isnan(xmin)]
+    xmin = xmin.astype(int)
+
+    # See if better max or min in region
+    yMax = np.array([])
+    xMax = np.array([])
+    for i in xmax:
+        indx = np.where(xmax == i)[0][0] + 1
+        try:
+            Y = y[i:xmax[indx]]
+            yMax = np.append(yMax, Y.max())
+            xMax = np.append(xMax, np.where(y == yMax[-1])[0][0])
+        except:
+            pass
+    yMin = np.array([])
+    xMin = np.array([])
+    for i in xmin:
+        indx = np.where(xmin == i)[0][0] + 1
+        try:
+            Y = y[i:xmin[indx]]
+            yMin = np.append(yMin, Y.min())
+            xMin = np.append(xMin, np.where(y == yMin[-1])[0][0])
+        except:
+            pass
+    if y[-1] > yMax[-1]:
+        yMax = np.append(yMax, y[-1])
+        xMax = np.append(xMax, x[-1])
+    if y[0] not in yMax:
+        yMax = np.insert(yMax, 0, y[0])
+        xMax = np.insert(xMax, 0, x[0])
+    if y[-1] < yMin[-1]:
+        yMin = np.append(yMin, y[-1])
+        xMin = np.append(xMin, x[-1])
+    if y[0] not in yMin:
+        yMin = np.insert(yMin, 0, y[0])
+        xMin = np.insert(xMin, 0, x[0])
+
+    # Plot results if desired
+    if charts is True:
+        from matplotlib.pyplot import plot, show, grid
+        plot(x, y)
+        plot(xMax, yMax, '-o')
+        plot(xMin, yMin, '-o')
+        grid(True)
+        show()
+    # Return arrays of critical points
+    return xMax, yMax, xMin, yMin
+
+def iterlines(x, window=30, charts=True):
+    """
+    Turn minitrends to iterative process more easily adaptable to
+    implementation in simple trading systems; allows backtesting functionality.
+    :param x: One-dimensional data set
+    :param window: How long the trendlines should be. If window < 1, then it
+                   will be taken as a percentage of the size of the data
+    :param charts: Boolean value saying whether to print chart to screen
+    """
+
+    import numpy as np
+
+    x = np.array(x)
+    n = len(x)
+    if window < 1:
+        window = int(window * n)
+    sigs = np.zeros(n, dtype=float)
+
+    i = window
+    while i != n:
+        if x[i] > max(x[i-window:i]): sigs[i] = 1
+        elif x[i] < min(x[i-window:i]): sigs[i] = -1
+        i += 1
+
+    xmin = np.where(sigs == -1.0)[0]
+    xmax = np.where(sigs == 1.0)[0]
+    ymin = x[xmin]
+    ymax = x[xmax]
+    if charts is True:
+        from matplotlib.pyplot import plot, grid, show
+        plot(x)
+        plot(xmin, ymin, 'ro')
+        plot(xmax, ymax, 'go')
+        grid(True)
+        show()
+
+    return sigs
+
+
+import yfinance as yf
+data = yf. download('AAPL','2016-01-01','2019-08-01')
+
+
+levels = supres(data['Low'], data['High'], n=28, min_touches = 2, stat_likeness_percent = 1.5, bounce_percent = 5)
+
+for column in levels.columns:
+    data[column] = levels[column]
+
+
+
+minitrends(data['Close'], window = 1/5, charts = True)
+
+data.to_excel('C:/Python/API/test.xlsx')
